a4c0cc9eb69425d3584a0528187dd25ea480ef2d
Ivan <djsupersky@gmail.com>
2022-04-22 14:40:13 +0300

    fix tiny bug in Pawn. I hope this is not a deadline delay :)

--- src/main/java/com/ivanxc/netcracker/lab/chapter9/programmingprojects/chess/figures/Pawn.java
+++ src/main/java/com/ivanxc/netcracker/lab/chapter9/programmingprojects/chess/figures/Pawn.java
@@ -5,22 +5,16 @@ import java.util.ArrayList;
 
 public class Pawn extends ChessPiece {
     private final int direction;
-    private final int pawsRow;
     private final int enPassantRow;
-    private final String regexToEnPassant;
 
     public Pawn(int row, int column, Color color, Chessboard board) {
         super(row, column, color, board);
         if (color == Color.WHITE) {
             direction = -1;
-            pawsRow = 6;
             enPassantRow = 3;
-            regexToEnPassant = "[a-h]7-[a-h]5";
         } else {
             direction = 1;
-            pawsRow = 1;
             enPassantRow = 4;
-            regexToEnPassant = "[a-h]2-[a-h]4";
         }
     }
 
@@ -31,12 +25,10 @@ public class Pawn extends ChessPiece {
         int direction = -1;
         char pawsRow = 6;
         char enPassantRow = 3;
-        String regexToEnPassant = "[a-h]7-[a-h]5";
         if (color == Color.BLACK) {
             direction = 1;
             pawsRow = 1;
             enPassantRow = 4;
-            regexToEnPassant = "[a-h]2-[a-h]4";
         }
 
         // Если впереди свободно
@@ -44,7 +36,7 @@ public class Pawn extends ChessPiece {
             availableCellsToMove.add(asString(column, row + direction));
         }
         // Если столбец больше 0 и слева сверху есть фигура
-        if (column > 1 && !board.isEmptyCell(column - 1, row + direction) &&
+        if (column > 0 && !board.isEmptyCell(column - 1, row + direction) &&
             board.getFigure(column - 1, row + direction).getColor() != color) {
             availableCellsToMove.add(asString(column - 1, row + direction));
         }
@@ -58,6 +50,7 @@ public class Pawn extends ChessPiece {
                      && board.isEmptyCell(column, row + direction * 2)) {
             availableCellsToMove.add(asString(column, row + direction * 2));
         }
+
         // Взятие на проходе
         String leftSide = (char)('a' + column - 1) + "" + (char)('8' - enPassantRow - direction * 2)
             + "-" + (char)('a' + column - 1) + (char)('8' - enPassantRow);
@@ -65,12 +58,12 @@ public class Pawn extends ChessPiece {
             + "-" + (char)('a' + column + 1) + (char)('8' - enPassantRow);
 
         if (row == enPassantRow && board.getLastMove().matches(leftSide)) {
-            if (column > 1 && board.getFigure(column - 1, row) instanceof Pawn) {
+            if (column > 0 && board.getFigure(column - 1, row) instanceof Pawn) {
                 availableCellsToMove.add(asString(column - 1, row + direction));
             }
         }
         if (row == enPassantRow && board.getLastMove().matches(rightSide)) {
-            if (column > 1 && board.getFigure(column + 1, row) instanceof Pawn) {
+            if (column < 7 && board.getFigure(column + 1, row) instanceof Pawn) {
                 availableCellsToMove.add(asString(column + 1, row + direction));
             }
         }
@@ -85,16 +78,15 @@ public class Pawn extends ChessPiece {
             + "-" + (char)('a' + column + 1) + (char)('8' - enPassantRow);
 
         if (row == enPassantRow && board.getLastMove().matches(leftSide)) {
-            if (column > 1 && board.getFigure(column - 1, row) instanceof Pawn) {
+            if (column > 0 && board.getFigure(column - 1, row) instanceof Pawn) {
                 return true;
             }
         }
         if (row == enPassantRow && board.getLastMove().matches(rightSide)) {
-            if (column > 1 && board.getFigure(column + 1, row) instanceof Pawn) {
+            if (column < 7 && board.getFigure(column + 1, row) instanceof Pawn) {
                 return true;
             }
         }
-
         return false;
     }
 
